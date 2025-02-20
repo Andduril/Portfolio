71378198e2d869a8721c4a5177c086c553603533
Andduril <73158252+Andduril@users.noreply.github.com>
2025-02-20 23:13:49 +0100

    feat: add Isagri section with animations; integrate new images and Card component; update Tailwind configuration

--- src/components/sections/experiences/Isagri.tsx
+++ src/components/sections/experiences/Isagri.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { useGSAP } from '@gsap/react';
+import Image from 'next/image';
 import { FC, RefObject, useRef, useState } from 'react';
 
+import Card from '@/components/ui/Card';
 import Reveal from '@/components/ui/Reveal';
 import gsap from '@/lib/gsapConfig';
 
+import Gobble from '../../../../public/Gobble_le_squelette.png';
+import IsagriLogo from '../../../../public/isagri.svg';
 import Section from '../Section';
 
 type IsagriProps = {
@@ -26,7 +30,7 @@ const IsagriPart1: FC<IsagriProps> = ({ ref }) => {
       scrollTrigger: {
         trigger: ref.current,
         start: 'top top',
-        end: '+300%',
+        end: '+100%',
         scrub: 1,
         pin: true,
       },
@@ -46,17 +50,21 @@ const IsagriPart1: FC<IsagriProps> = ({ ref }) => {
   return (
     <Section ref={ref} className="relative flex flex-col">
       {/* Animation*/}
-      <Section ref={animationRef} className="relative flex flex-row items-center">
+      <Section ref={animationRef} className="relative flex flex-row-reverse items-center">
         <div className="w-1/2 flex flex-col items-center">
-          <Reveal trigger={reveal}>
-            <h1 className="text-7xl leading-normal">{'Isagri'}</h1>
+          <Reveal trigger={reveal} duration={2000}>
+            {/* <h1 className="text-7xl leading-normal">{'Isagri'}</h1> */}
+            <div className="flex flex-col items-center">
+              <Image src={IsagriLogo} alt="isagri" height={75} />
+              <h2 className="text-3xl leading-normal">{'Développeur Fullstack'}</h2>
+            </div>
           </Reveal>
         </div>
         <div className="w-1/2" />
       </Section>
 
       {/* Element absolute */}
-      <div className="absolute top-[50vh] left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row-reverse w-1/5">
+      <div className="absolute top-[50vh] left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row w-1/5">
         <Reveal trigger={reveal}>
           <h3 ref={dateRef} className="text-3xl">
             {'Sept 2025'}
@@ -72,7 +80,9 @@ const IsagriPart1: FC<IsagriProps> = ({ ref }) => {
 };
 
 const IsagriPart2: FC<IsagriProps> = ({ ref }) => {
-  const squareRef = useRef(null);
+  const parcelRef = useRef(null);
+  const scanRef = useRef(null);
+  const gobbleMask = useRef(null);
 
   useGSAP(() => {
     gsap
@@ -85,15 +95,34 @@ const IsagriPart2: FC<IsagriProps> = ({ ref }) => {
           pin: true,
         },
       })
-      .from(squareRef.current, { rotate: 360 });
+      .from(parcelRef.current, { opacity: 0, duration: 1 }, '<')
+      .to(parcelRef.current, { rotateY: 15, rotateZ: -5, rotateX: 5, duration: 3 }, '<')
+      .to(scanRef.current, { top: '100%', duration: 5 }, '<')
+      .to(gobbleMask.current, { y: '100%', duration: 5 }, '<');
   });
 
   return (
-    <Section ref={ref} className="relative flex flex-col">
-      <div
-        ref={squareRef}
-        className="absolute top-[50vh] left-1/2 -translate-x-1/2 -translate-y-1/2 size-14 bg-green-400"
-      />
+    <Section ref={ref} className="relative flex flex-row">
+      <div className="w-1/2 flex flex-col items-center justify-center">
+        {/* Parcelle */}
+        <div className="perspective-400">
+          <div ref={parcelRef} className="relative bg-green-900 w-64 h-96 overflow-hidden">
+            <Image src={Gobble} alt="Goble" layout="fill" objectFit="cover" />
+            <div ref={gobbleMask} className="absolute size-full bg-green-400" />
+            <div ref={scanRef} className="absolute w-full top-0 h-2 bg-blue-400" />
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 flex flex-col items-center justify-center">
+        <Card className="max-w-12">
+          <h1 className="text-4xl">{'Isagri'}</h1>
+          <p className="text-lg">
+            {
+              "Développeur Fullstack chez Isagri, je travaille sur la refonte de l'application web de gestion de la paie."
+            }
+          </p>
+        </Card>
+      </div>
     </Section>
   );
 };
