30da2d797f3fb7b588bde4d935e22c3f5b8e4dc0
Anddy Labrut <73158252+Andduril@users.noreply.github.com>
2025-04-04 22:31:12 +0200

    feat: add under construction message to TopBar and update layout

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,24 +5,29 @@ const TopBar = async () => {
   const t = await getTranslations('Navigation');
 
   return (
-    <header className="sticky bg-transparent top-0 left-0 w-full flex flex-row items-center px-10 z-20">
-      <h1 className="grow text-2xl">{t('mainButton')}</h1>
-      <nav className="p-4">
-        <ul className="flex flex-row gap-8 justify-evenly">
-          <Link href={'/'} className="text-gray-400 hover:text-white">
-            {t('home')}
-          </Link>
-          <Link href={'/projects'} className="text-gray-400 hover:text-white">
-            {t('projects')}
-          </Link>
-          <Link href={'/skills'} className="text-gray-400 hover:text-white">
-            {t('skills')}
-          </Link>
-          <Link href={'/contact'} className="text-gray-400 hover:text-white">
-            {t('contact')}
-          </Link>
-        </ul>
-      </nav>
+    <header className="sticky bg-transparent top-0 left-0 w-full z-20">
+      <div className="flex flex-row items-center justify-center bg-yellow-500 p-2">
+        <h5 className="text-2xl text-black">{t('underConstruction')}</h5>
+      </div>
+      <div className="flex flex-row items-center px-10">
+        <h1 className="grow text-2xl">{t('mainButton')}</h1>
+        <nav className="p-4">
+          <ul className="flex flex-row gap-8 justify-evenly">
+            <Link href={'/'} className="text-gray-400 hover:text-white">
+              {t('home')}
+            </Link>
+            <Link href={'/projects'} className="text-gray-400 hover:text-white">
+              {t('projects')}
+            </Link>
+            <Link href={'/skills'} className="text-gray-400 hover:text-white">
+              {t('skills')}
+            </Link>
+            <Link href={'/contact'} className="text-gray-400 hover:text-white">
+              {t('contact')}
+            </Link>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
