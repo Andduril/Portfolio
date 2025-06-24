32671c9844b092bdb37113d457aceef1898db284
andduril <labrut.anddy@gmail.com>
2025-06-24 12:02:57 +0200

    feat: add ESLint configuration to ignore errors during build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+  },
 };
 
 export default withNextIntl(nextConfig);
